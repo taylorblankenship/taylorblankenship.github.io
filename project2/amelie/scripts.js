fd5192cf0a2a159103cd11cf33df3a7896110902
taylorblankenship <taylorblankenship66@gmail.com>
2025-03-02 08:16:06 -0500

    project 2 examples

---
+++ project2/amelie/scripts.js
@@ -0,0 +1,9 @@
+const modalContainerRef = document.getElementById("modalContainer");
+
+function openModal() {
+    modalContainerRef.style.display = "flex";
+}
+
+function closeModal() {
+    modalContainerRef.style.display = "none";
+}
